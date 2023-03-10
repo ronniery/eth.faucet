3a5191fe069a9b818ca8b2914f2afb638fbfaddb
Ronniery Borges Correa <ronnisti@gmail.com>
2023-03-10 15:34:42 +0000

    chore: added to the project ganache and truffle to deploy contracts on the fly

---
+++ contracts/Faucet.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+  // Storage variables, it will keep some states over the time
+  uint public funds = 1000 ether;
+}
