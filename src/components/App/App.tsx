b5512c8f5026b1f8683ecd57d5e5039bdb664418
Ronniery Borges Correa <ronnisti@gmail.com>
2023-03-29 17:35:22 +0100

    chore: added the initial UI structure to connect with the wallet

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,32 +1,55 @@
 import { useEffect, useState } from "react";
+import { JSONRPCMethodsRestricted } from "../../constants";
 import useWeb3 from "../../hooks/useWeb3";
 import "./App.css";
 
 function App() {
   const web3Api = useWeb3();
 
-  const [account, setAccount] = useState<string>('');
+  const [account, setAccount] = useState<string>("");
 
   useEffect(() => {
     const getAccounts = async () => {
-      if(web3Api.web3) {
+      if (web3Api.web3) {
         const accounts = await web3Api.web3.eth.getAccounts();
         setAccount(accounts.at(0) as string);
+         console.log(account);
       }
-    }
+    };
 
     getAccounts();
   }, [web3Api.web3]);
 
+  const connectWallet = () =>
+    web3Api.provider?.request<string[]>({
+      method: JSONRPCMethodsRestricted.ETH_REQUEST_ACCOUNTS,
+    });
+
   return (
     <div className="faucet-wrapper">
       <div className="faucet">
-        <div className="balance-view is-size-2">
+        <div className="user-account is-flex is-flex-direction-row">
+          <span className="is-align-self-center mr-2">
+            <strong> Account: </strong>
+          </span>
+
+          {account ? (
+            <h1>{account}</h1>
+          ) : (
+            <button className="button" onClick={connectWallet}>
+              Connect Wallet
+            </button>
+          )}
+        </div>
+        <div className="balance-view is-size-2 mb-3">
           Current Balance: <strong>10</strong> ETH
         </div>
-        <h1>{account}</h1>
-        <button className="btn mr-2"> Donate </button>
-        <button className="btn"> Withdraw </button>
+        {account && (
+          <>
+            <button className="button is-outlined mr-2"> Donate </button>
+            <button className="button is-outlined"> Withdraw </button>
+          </>
+        )}
       </div>
     </div>
   );
--- src/hooks/useWeb3.tsx
+++ src/hooks/useWeb3.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 
 import detectEthereumProvider from "@metamask/detect-provider";
+import { MetaMaskInpageProvider } from "@metamask/providers";
 import { provider as Web3CoreProvider } from "web3-core";
 import Web3 from "web3";
 
 type Web3Provider = {
-  provider: Web3CoreProvider;
+  provider: MetaMaskInpageProvider | null;
   web3: Web3 | null;
 };
 
@@ -17,11 +18,11 @@ const useWeb3 = (): Web3Provider => {
 
   useEffect(() => {
     const loadProvider = async () => {
-      const provider = await detectEthereumProvider<Web3CoreProvider>();
+      const provider = await detectEthereumProvider<MetaMaskInpageProvider>();
 
       if (provider) {
         setApi({
-          web3: new Web3(provider),
+          web3: new Web3(provider as Web3CoreProvider),
           provider,
         });
       } else {
