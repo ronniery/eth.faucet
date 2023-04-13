3dfb9fe163f4ef9aae6c3f7b7c0cd517b96ac31e
Ronniery Borges Correa <ronnisti@gmail.com>
2023-04-13 20:12:48 +0100

    chore: updated install.sh to log some outputs and silenced some steps

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Bash colors
+RED="\033[0;31m"
+YELLOW="\033[0;33m"
+
+# No color
+NO_COLOR="\033[0m"
+
 # Warn user about meta mask
-printf "%b" "\033[0;31m You must install Metamask to use that application https://metamask.io/download/\e[0m\n"
+printf "%b" "$RED → You must install Metamask to use that application https://metamask.io/download/$NO_COLOR\n"
 
 # Working directory
 workdir="/tmp/ronniery"
@@ -12,13 +19,19 @@ rm -rf "$workdir" > /dev/null 2>&1
 # Lets setup the directory path
 mkdir -p "$workdir"
 
+# Clonning locally the eth.faucet
+printf "%b" "$YELLOW → Clonning eth.faucet project locally...$NO_COLOR\n"
+
 # Clone the project locally
 git clone https://github.com/ronniery/eth.faucet.git --quiet $workdir &> /dev/null
 
 # Go to inside the cloned project
 cd "$workdir"
 
-# Make the docker composer build all necessary stuff
+# Building images with docker compose
+printf "%b" "$YELLOW → Building docker images with compose...$NO_COLOR\n"
+
+# Make the docker compose build all necessary stuff
 docker compose up -d
 
 # Waiting some seconds before next step
@@ -26,12 +39,24 @@ printf "Waiting 5 seconds, before install truffle and start to deploy contracts.
 sleep 5
 
 # Install truffle and migrate contracts
-npm install truffle
-npx truffle compile
-npx truffle migrate
+printf "%b" "$YELLOW → Installing truffle on your system...$NO_COLOR\n"
+
+# Installing npm package, truffle
+npm install --silent truffle
+
+# Compiling smartcontracts with truffle
+printf "%b" "$YELLOW    - Compiling all contracts!$NO_COLOR\n"
+npx truffle compile &> /dev/null
+
+# Deploying compiled contracts within ganache network
+printf "%b" "$YELLOW    - Deploying all contracts inside Ganache!$NO_COLOR\n"
+npx truffle migrate &> /dev/null
 
 # Cleaning stuff
-npm uninstall truffe
+printf "%b" "$YELLOW → Removing truffle package $NO_COLOR\n"
+npm uninstall --silent truffe
+
+# Removing project dir
 rm -rf $workdir
 
 # Print some usefull messages
@@ -40,4 +65,4 @@ printf "%b" "\x1B[32m✔ Docker images created\e[0m \n"
 printf "%b" "\x1B[32m✔ Migrated all contracts\e[0m \n\n"
 printf "Now you can access those endpoints:\n"
 printf "   - http://localhost:6002 / If you want to access the Ethereum faucet application\n"
-printf "   - http://localhost:6003 / If you want to access all available accounts with their secret keys\n"
+printf "   - http://localhost:6003/accounts.json / If you want to access all available accounts with their secret keys\n"
